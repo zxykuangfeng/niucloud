2f3627fb09d44b179350ee21eba37e590a2d9a96
18036291231@163.com <18036291231@163.com>
2025-06-26 11:10:44 +0800

    test

--- core/pay/Douyinpay.php
+++ core/pay/Douyinpay.php
@@ -6,82 +6,82 @@ use think\Response;
 
 class Douyinpay extends BasePay
 {
-    protected function initialize(array $config = [])
+    public function initialize(array $config = [])
     {
         $this->config = $config;
     }
 
-    protected function web(array $params)
+    public function web(array $params)
     {
         throw new PayException('DOUYINPAY_NOT_SUPPORTED');
     }
 
-    protected function wap(array $params)
+    public function wap(array $params)
     {
         return $this->web($params);
     }
 
-    protected function app(array $params)
+    public function app(array $params)
     {
         throw new PayException('DOUYINPAY_NOT_SUPPORTED');
     }
 
-    protected function mini(array $params)
+    public function mini(array $params)
     {
         return $this->app($params);
     }
 
-    protected function pos(array $params)
+    public function pos(array $params)
     {
         throw new PayException('DOUYINPAY_NOT_SUPPORTED');
     }
 
-    protected function scan(array $params)
+    public function scan(array $params)
     {
         throw new PayException('DOUYINPAY_NOT_SUPPORTED');
     }
 
-    protected function transfer(array $params)
+    public function transfer(array $params)
     {
         throw new PayException('DOUYINPAY_NOT_SUPPORTED');
     }
 
-    protected function mp(array $params)
+    public function mp(array $params)
     {
         throw new PayException('DOUYINPAY_NOT_SUPPORTED');
     }
 
-    protected function close(string $out_trade_no)
+    public function close(string $out_trade_no)
     {
         return true;
     }
 
-    protected function refund(array $params)
+    public function refund(array $params)
     {
         throw new PayException('DOUYINPAY_NOT_SUPPORTED');
     }
 
-    protected function notify(string $action, callable $callback)
+    public function notify(string $action, callable $callback)
     {
         return Response::create('success', 'html');
     }
 
-    protected function getOrder(array $params)
+    public function getOrder(array $params)
     {
         return [];
     }
 
-    protected function getRefund(string $out_trade_no, ?string $refund_no)
+    public function getRefund(string $out_trade_no, ?string $refund_no)
     {
         return [];
     }
 
-    protected function getTransfer(string $transfer_no, $out_transfer_no = '')
+    public function getTransfer(string $transfer_no, $out_transfer_no = '')
     {
         return [];
     }
 
-    protected function transferCancel(array $params)
+    public function transferCancel(array $params)
     {
         return [];
     }
