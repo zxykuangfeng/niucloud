182c40662aace8dd5419e9f4f69ea0ce53357a6c
18036291231@163.com <18036291231@163.com>
2025-07-03 09:33:23 +0800

    授权链接功能

---
+++ app/service/api/toutiao/CoreToutiaoConfigService.php
@@ -0,0 +1,40 @@
+<?php
+namespace app\service\core\toutiao;
+
+use app\dict\sys\ConfigKeyDict;
+use app\model\sys\SysConfig;
+use app\service\core\sys\CoreConfigService;
+use core\base\BaseCoreService;
+use think\Model;
+
+class CoreToutiaoConfigService extends BaseCoreService
+{
+    public function getToutiaoConfig(int $site_id)
+    {
+        $info = (new CoreConfigService())->getConfig($site_id, ConfigKeyDict::TOUTIAO)['value'] ?? [];
+        return [
+            'app_id' => $info['app_id'] ?? '',
+            'app_secret' => $info['app_secret'] ?? ''
+        ];
+    }
+
+    public function setToutiaoConfig(int $site_id, array $data)
+    {
+        $config = [
+            'app_id' => $data['app_id'] ?? '',
+            'app_secret' => $data['app_secret'] ?? ''
+        ];
+        return (new CoreConfigService())->setConfig($site_id, ConfigKeyDict::TOUTIAO, $config);
+    }
+
+        /**
+     * 获取抖音平台回调地址
+     * @return array
+     */
+    public function getStaticInfo(): array
+    {
+        return [
+            'auth_serve_url' => (string)url('/adminapi/toutiao/server', [], '', true)
+        ];
+    }
+}
